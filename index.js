9af7b7a32b89887a249f3dbeffb8571bb842c232
Fire Fish <mailsodd@gmail.com>
2022-02-12 20:17:30 +0000

    quack

--- index.js
+++ index.js
@@ -102,8 +102,8 @@ client.once("ready", async () => {
 });
 
 client.on("messageCreate", async msg => {
-  await db.sendCommand("HSETNX", msg.author.id, "balance", 20, {
-//    NX: true,
+  await db.sendCommand.hSet(msg.author.id, "balance", 20, {
+    NX: true,
   });
 
   console.log(`${msg.author.username}#${msg.author.discriminator}: ${msg.content}`);
